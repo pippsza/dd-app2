d119c0ce5556427037c48499f7821c68bbade2da
pippsza <bastahov18@gmail.com>
2025-05-26 19:43:47 +0300

    123

--- app/components/teeContainer.tsx
+++ app/components/teeContainer.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 import {
   responsiveHeight as rh,
@@ -12,137 +12,197 @@ import { useTranslation } from "react-i18next";
 
 export default function TeeContainer({ data, online }: any) {
   const { t } = useTranslation();
-  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const { isDarkMode } = useContext(ThemeContext);
+
   const style = StyleSheet.create({
-    container: {
+    mainContainer: {
+      flexDirection: "column",
+      // alignItems: "center",
+      width: rw(100),
+      paddingVertical: rh(2),
+      // paddingHorizontal: rw(2),
+    },
+    rowContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
       alignItems: "center",
-      gap: rh(2),
-      // position: "absolute",
-      // left: 100,
+      width: rw(90),
+      paddingVertical: rh(1),
+      // backgroundColor: "green",
+    },
+    statusBlock: {
+      flex: 1,
+      alignItems: "flex-start",
+      paddingLeft: rw(3),
+      paddingVertical: rh(1),
+      backgroundColor: isDarkMode ? "white" : "#272727",
+      borderColor: isDarkMode ? "black" : "white",
+      borderWidth: rw(0.4),
+      borderLeftWidth: 0,
+      borderRadius: rw(6),
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+    },
+    rightContainer: {
+      backgroundColor: isDarkMode ? "white" : "#272727",
+      paddingVertical: rh(1),
+      paddingLeft: rw(6),
+      paddingRight: rw(4),
+      borderWidth: rw(0.4),
+      borderRightWidth: 0,
+      borderTopLeftRadius: rw(4),
+      borderBottomLeftRadius: rw(4),
+    },
+    teeBlock: {
+      flexDirection: "row",
+      alignItems: "center",
+      width: rw(100),
+      gap: rw(5),
+      justifyContent: "space-between",
+      // backgroundColor: "purple",
+    },
+    favoritesBlock: {
+      flex: 1,
+      alignItems: "flex-end",
+    },
+    profileBlock: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      width: rw(100),
+      // backgroundColor: isDarkMode ? "white" : "#272727",
+    },
+    name: {
+      backgroundColor: isDarkMode ? "white" : "#272727",
+      borderWidth: rw(0.4),
+      borderLeftWidth: 0,
+      borderRadius: rw(3),
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
+      paddingHorizontal: rw(4),
+    },
+    pts: {
+      backgroundColor: isDarkMode ? "white" : "#272727",
+      borderWidth: rw(0.4),
+      borderRightWidth: 0,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: rw(3),
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
+      paddingHorizontal: rw(4),
     },
     longText: {
-      textAlign: "center",
-      fontSize: rf(Math.max(3 - online.server?.length * 0.05, 1.5)),
+      fontSize: rf(Math.max(3 - online.server?.length * 0.03, 1.5)),
       color: isDarkMode ? "black" : "white",
+      paddingLeft: rw(2.6),
     },
     regText: {
-      textAlign: "center",
       fontSize: rf(2),
       color: isDarkMode ? "black" : "white",
     },
     bigText: {
-      textAlign: "center",
-      fontSize: rf(3),
+      fontSize: rf(4),
+      // textAlign: "center",
       color: isDarkMode ? "black" : "white",
     },
     smallText: {
-      textAlign: "center",
-      fontSize: rf(1.8),
-      color: isDarkMode ? "black" : "white",
-    },
-    teeContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-
-      justifyContent: "space-between",
-      paddingHorizontal: rw(5),
-      width: rw(100),
-      // height: rh(32),
-    },
-    img: {
-      width: rw(20),
-      height: rw(20),
-    },
-    rightText: {
       fontSize: rf(2),
-      textAlign: "center",
       color: isDarkMode ? "black" : "white",
     },
-    rightSmallText: {
-      fontSize: rf(1.8),
-      textAlign: "center",
+    clanText: {
+      fontSize: rf(2.3),
+      paddingLeft: rw(4),
       color: isDarkMode ? "black" : "white",
     },
-    Online: { textAlign: "center", fontSize: rf(3), color: "green" },
-    Offline: { textAlign: "center", fontSize: rf(5), color: "red" },
-    AFK: { textAlign: "center", fontSize: rf(3), color: "blue" },
-    Designer: { textAlign: "center", fontSize: rf(5), color: "orange" },
-    Developer: { textAlign: "center", fontSize: rf(5), color: "#c9007c" },
-    teeWrapper: {
-      position: "absolute",
-      right: rw(-19),
-    },
-    bigTeeWrapper: {
-      height: rh(17),
+    Online: { color: "green", fontSize: rf(4) },
+    Offline: { color: "red", fontSize: rf(6) },
+    AFK: { color: "blue" },
+    Designer: { color: "orange" },
+    Developer: { color: "#c9007c" },
+    teeSize: {
+      width: rh(10),
+      height: rh(10),
     },
   });
 
   return (
-    <>
-      <View style={style.teeContainer}>
-        <View>
-          <Text style={style.regText}>{t("teeContainer.bestFriend")}:</Text>
-          <Text style={style.smallText}>
-            {data.favourite_teammates.length > 0
-              ? data.favourite_teammates[0].name
-              : t("teeContainer.none")}
-          </Text>
-        </View>
-        <View style={style.container}>
-          {online.status === "Designer" && "Developer" ? (
-            <Text style={style[online.status]}>{online.status}</Text>
+    <View style={style.mainContainer}>
+      {/* Первый ряд - Статус */}
+      <View style={style.rowContainer}>
+        <View style={style.statusBlock}>
+          {online.status === "Designer" || online.status === "Developer" ? (
+            <Text style={[style.regText, style[online.status]]}>
+              {online.status}
+            </Text>
           ) : (
-            <View>
-              <Text style={style[online.status]}>
+            <>
+              <Text style={[style.regText, style[online.status]]}>
                 {online.status === "Offline"
                   ? t("teeContainer.offline")
                   : online.status === "Online"
                   ? t("teeContainer.online")
                   : t("teeContainer.AFK")}
               </Text>
-
-              {online.mapName ? (
+              {online.mapName && (
                 <>
                   <Text style={style.longText}>
                     {t("teeContainer.playingOn")}: {online.mapName}
                   </Text>
                   <Text style={style.longText}>{online.server}</Text>
                 </>
-              ) : null}
-            </View>
+              )}
+            </>
           )}
-          <View style={style.bigTeeWrapper}>
-            <View style={style.teeWrapper}>
-              <Tee width={rh(4)} source={data.profile.skin_name}></Tee>
-            </View>
-          </View>
-          <View>
-            <Text style={style.bigText}>{data.profile.name}</Text>
-            <Text style={style.smallText}>{data.profile.clan}</Text>
-            <Text style={style.regText}>
-              {data.profile.points} {t("teeContainer.PTS")}
-            </Text>
-          </View>
         </View>
-        <View>
-          <View>
-            <Text style={style.rightText}>{t("teeContainer.favMap")}:</Text>
-            <Text style={style.rightSmallText}>
-              {data.most_played_maps.lenght > 0
-                ? data.most_played_maps[0].map_name
-                : t("teeContainer.none")}
+      </View>
+
+      {/* Второй ряд - Тишка и любимые */}
+      <View style={style.rowContainer}>
+        <View style={style.teeBlock}>
+          <Tee width={rh(4)} source={data.profile.skin_name} />
+          <View style={style.rightContainer}>
+            <Text style={style.regText}>
+              {t("teeContainer.bestFriend")}:{" "}
+              <Text style={style.smallText}>
+                {data.favourite_teammates.length > 0
+                  ? data.favourite_teammates[0].name
+                  : t("teeContainer.none")}
+              </Text>
             </Text>
-          </View>
-          <View>
-            <Text style={style.rightText}>{t("teeContainer.favRegion")}:</Text>
-            <Text style={style.rightSmallText}>
-              {data.most_played_locations.lenght > 0
-                ? data.most_played_locations[0].key.toUpperCase()
-                : t("teeContainer.none")}
+            <Text style={style.regText}>
+              {t("teeContainer.favMap")}:{" "}
+              <Text style={style.smallText}>
+                {data.most_played_maps.length > 0
+                  ? data.most_played_maps[0].map_name
+                  : t("teeContainer.none")}
+              </Text>
+            </Text>
+            <Text style={style.regText}>
+              {t("teeContainer.favRegion")}:{" "}
+              <Text style={style.smallText}>
+                {data.most_played_locations.length > 0
+                  ? data.most_played_locations[0].key.toUpperCase()
+                  : t("teeContainer.none")}
+              </Text>
             </Text>
           </View>
         </View>
       </View>
-    </>
+
+      {/* Третий ряд - Профиль */}
+      <View style={style.profileBlock}>
+        <View style={style.name}>
+          <Text style={style.bigText}>{data.profile.name}</Text>
+          {data.profile.clan ? (
+            <Text style={style.clanText}>{data.profile.clan}</Text>
+          ) : null}
+        </View>
+        <View style={style.pts}>
+          <Text style={style.bigText}>
+            {data.profile.points} {t("teeContainer.PTS")}
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 }
